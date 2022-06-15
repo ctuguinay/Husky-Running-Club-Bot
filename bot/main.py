e12077a48674d1faab60155563ed53733176d7ad
ctuguina <ctuguina@uw.edu>
2022-06-15 15:24:31 -0700

    add test embed
    - add green lake zoo loop test embed

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import os
+import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
@@ -9,8 +10,12 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command(name="test")
+@commands.has_role("Officer")
 async def ping(ctx):
-    await ctx.send("pong")
+    embed=discord.Embed(title="Green Lake Zoo Loop", url="https://www.mapmyfitness.com/routes/view/embedded/5050970416?width=600&height=400&&line_color=E68006c6&rgbhex=c60680&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&show_marker_every=1&last_updated=undefined'",
+    description="Test Embed", color=0xFF5733)
+    embed.set_thumbnail(url="imgs/zoo_loop.jpeg")
+    await ctx.send(embed=embed)
 
 if __name__ == "__main__":
     bot.run(TOKEN)
