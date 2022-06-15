3fff5f0491c11818479b04aec366bf8c28ffd55d
ctuguina <ctuguina@uw.edu>
2022-06-15 15:41:04 -0700

    add url attachment

--- bot/main.py
+++ bot/main.py
@@ -14,7 +14,7 @@ async def on_ready():
 async def ping(ctx):
     embed=discord.Embed(title="Green Lake Zoo Loop", url="https://www.mapmyfitness.com/routes/view/embedded/5050970416?width=600&height=400&&line_color=E68006c6&rgbhex=c60680&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&show_marker_every=1&last_updated=undefined'",
     description="Test Embed", color=0xFF5733)
-    embed.set_thumbnail(file=discord.File("imgs/zoo_loop.png"))
+    embed.set_thumbnail(url="attachment://imgs/zoo_loop.png")
     await ctx.send(embed=embed)
 
 if __name__ == "__main__":
